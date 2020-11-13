1783177ab1a0fa8d55b0f4f456c1e3df19d86f7a
Sakshi-dalal <71662168+Sakshi-dalal@users.noreply.github.com>
2020-11-13 11:21:32 -0500

    Email verification

---
+++ WebForm1.aspx.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Net.Mail;
+using System.Net;
+
+namespace WebApplication3
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        static String activationcode;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            activationcode = random.Next(1001, 9999).ToString();
+            String query = "insert into emaildetails(FirstName,Emailid,Status,activationcode) values('" + TextBox1.Text + "','" + TextBox2.Text + "','Unverified','" + activationcode + "')";
+            String mycon = "DESKTOP-S2EPCEP\\SQLEXPRESS; Initial Catalog=Iwin; Integrated Security=true";
+            SqlConnection con = new SqlConnection(mycon);
+            con.Open();
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = query;
+            cmd.Connection = con;
+            cmd.ExecuteNonQuery();
+            sendcode();
+            Response.Redirect("emailactivate.aspx?emailadd=" + TextBox2.Text);
+        }
+
+        private void sendcode()
+        {
+            SmtpClient smtp = new SmtpClient();
+            smtp.Host = "smtp.gmail.com";
+            smtp.Port = 587;
+            smtp.Credentials = new System.Net.NetworkCredential("orderiwin07@gmail.com", "roboCON2017@");
+            smtp.EnableSsl = true;
+            MailMessage msg = new MailMessage();
+            msg.Subject = "Activation Code to Verify Email Address";
+            msg.Body = "Dear " + TextBox1.Text + ", Your Activation Code is  " + activationcode + "\n\n\nThanks & Regards\nHariti Study Hub Team";
+            string toaddress = TextBox2.Text;
+            msg.To.Add(toaddress);
+            string fromaddress = "Iwin <orderiwin07@gmail.com>";
+            msg.From = new MailAddress(fromaddress);
+            try
+            {
+                smtp.Send(msg);
+
+
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+    }
+}
+
---
+++ emailactivate.aspx.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace WebApplication3
+{
+    public partial class emailactivate : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Label3.Text = "Your Email is " + Request.QueryString["emailadd"].ToString() + " , Kindly Check Your Mail Inbox For Activation Code";
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            String mycon = "DESKTOP-S2EPCEP\\SQLEXPRESS; Iwin; Integrated Security=True";
+            String myquery = "Select * from EmailDetails where Emailid='" + Request.QueryString["emailid"] + "'";
+            SqlConnection con = new SqlConnection(mycon);
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = myquery;
+            cmd.Connection = con;
+            SqlDataAdapter da = new SqlDataAdapter();
+            da.SelectCommand = cmd;
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            if (ds.Tables[0].Rows.Count > 0)
+            {
+                String activationcode;
+                activationcode = ds.Tables[0].Rows[0]["activationcode"].ToString();
+                if (activationcode == TextBox1.Text)
+                {
+                    changestatus();
+                    Label4.Text = "Your Email Has Been Verified Successfully";
+                }
+                else
+                {
+                    Label4.Text = "You Have Entered Invalid Activation Code, Kindly Check Your Mail Inbox";
+                }
+            }
+
+            con.Close();
+        }
+        private void changestatus()
+        {
+            String mycon = "DESKTOP-S2EPCEP\\SQLEXPRESS; Initial Catalog=Iwin; Integrated Security=True";
+            String updatedata = "Update emaildetails set status='Verified' where emailaddress='" + Request.QueryString["emailadd"] + "'";
+            SqlConnection con = new SqlConnection(mycon);
+            con.Open();
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = updatedata;
+            cmd.Connection = con;
+            cmd.ExecuteNonQuery();
+        }
+    }
+
+}
+
+
