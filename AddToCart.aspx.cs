7ce1cef0e0dfdfefaca31f507b29389f03908e2e
Lourdureddy-Allam <71672830+Lourdureddy-Allam@users.noreply.github.com>
2020-11-12 20:17:11 -0500

    CART created
    created cart, can add items to cart, showed no.of items in cart, redirected from cart to main page and and main page to cart.

---
+++ AddToCart.aspx.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+public partial class AddToCart : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            DataTable dt = new DataTable();
+            DataRow dr;
+            dt.Columns.Add("sno");
+            dt.Columns.Add("product_id");
+            dt.Columns.Add("product_name");
+            dt.Columns.Add("product_price");
+            dt.Columns.Add("productimage");
+
+
+
+
+            if (Request.QueryString["id"] != null)
+            {
+                if (Session["Buyitems"] == null)
+                {
+
+                    dr = dt.NewRow();
+                    String mycon = "Data Source=DESKTOP-MFL6VGP\\SQLEXPRESS06;Initial Catalog=iwin;Integrated Security=True";
+                    SqlConnection scon = new SqlConnection(mycon);
+                    String myquery = "select * from dbo.tbl_lookup_product where product_id=" + Request.QueryString["id"];
+                    SqlCommand cmd = new SqlCommand();
+                    cmd.CommandText = myquery;
+                    cmd.Connection = scon;
+                    SqlDataAdapter da = new SqlDataAdapter();
+                    da.SelectCommand = cmd;
+                    DataSet ds = new DataSet();
+                    da.Fill(ds);
+                    dr["sno"] = 1;
+                    dr["product_id"] = ds.Tables[0].Rows[0]["product_id"].ToString();
+                    dr["product_name"] = ds.Tables[0].Rows[0]["product_name"].ToString();
+                    dr["product_price"] = ds.Tables[0].Rows[0]["product_price"].ToString();
+                    dt.Rows.Add(dr);
+                    GridView1.DataSource = dt;
+                    GridView1.DataBind();
+                    Session["buyitems"] = dt;
+                }
+                else
+                {
+
+                    dt = (DataTable)Session["buyitems"];
+                    int sr;
+                    sr = dt.Rows.Count;
+
+                    dr = dt.NewRow();
+                    String mycon = "Data Source=DESKTOP-MFL6VGP\\SQLEXPRESS06;Initial Catalog=iwin;Integrated Security=True";
+                    SqlConnection scon = new SqlConnection(mycon);
+                    String myquery = "select * from dbo.tbl_lookup_product where product_id=" + Request.QueryString["id"];
+                    SqlCommand cmd = new SqlCommand();
+                    cmd.CommandText = myquery;
+                    cmd.Connection = scon;
+                    SqlDataAdapter da = new SqlDataAdapter();
+                    da.SelectCommand = cmd;
+                    DataSet ds = new DataSet();
+                    da.Fill(ds);
+                    dr["sno"] = sr + 1;
+                    dr["product_id"] = ds.Tables[0].Rows[0]["product_id"].ToString();
+                    dr["product_name"] = ds.Tables[0].Rows[0]["product_name"].ToString();
+                    dr["product_price"] = ds.Tables[0].Rows[0]["product_price"].ToString();
+                    dt.Rows.Add(dr);
+                    GridView1.DataSource = dt;
+                    GridView1.DataBind();
+                    Session["buyitems"] = dt;
+
+                }
+            }
+            else
+            {
+                dt = (DataTable)Session["buyitems"];
+                GridView1.DataSource = dt;
+                GridView1.DataBind();
+
+            }
+
+        }
+    }
+}
