39b6210f50c80a2f76f5b20ca98ee04830fe8423
Gayathri-Kolanu <71561916+Gayathri-Kolanu@users.noreply.github.com>
2020-11-18 05:26:26 +0530

    Dual Account for User
    Feasibility for a user to do switch between 2 profiles- buyer and seller

---
+++ Default2.aspx
@@ -0,0 +1,40 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default2.aspx.cs" Inherits="Default2" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <table align="center">
+                <tr>
+                    <td>Username</td>
+                    <td>
+                        <asp:TextBox ID="txtuser" runat="server"></asp:TextBox></td>
+                </tr>
+                <tr>
+                    <td>Password</td>
+                    <td>
+                        <asp:TextBox ID="txtpass" runat="server"></asp:TextBox></td>
+                </tr>
+                <tr>
+                    <td>Select User Type</td>
+                    <td>
+                        <asp:DropDownList ID="DropDownList1" runat="server" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged">
+                            <asp:ListItem>Buyer</asp:ListItem>
+                            <asp:ListItem>Seller</asp:ListItem>
+                        </asp:DropDownList></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td>
+                        <asp:Button ID="Button1" runat="server" Text="login" OnClick="Button1_Click" /></td>
+                </tr>
+                </table>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Default2.aspx 1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+
+namespace userlogin {
+    public partial class Default2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e) {
+            SqlConnection con = new SqlConnection("Data Source = DESKTOP-O9MNC2S\SQLEXPRESS01'; Initial Catalog = userlogin; Integrated Security = True");
+            SqlDataAdapter sda = new SqlDataAdapter("select * from userlogin where username'" + txtuser.Text + "' and password = '" + txtpass.Text + " and usertype = '" + DropDownList1.SelectedItem.ToString() + "'", con);
+            DataTable dt = new DataTable();
+
+            sda.Fill(dt);
+            if (dt.Rows.Count > 0)
+            {
+                Response.Write("<script>('you are logined as " + dt.Rows[0][2] + "')</script>");
+                if (DropDownList1_SelectedIndexChanged == 0)
+                {
+                    Server.Transfer("buyer.aspx");
+                } else if (DropDownList1_SelectedIndexChanged == 1)
+                {
+                    Server.Transfer("Sellerpage.aspx");
+                }
+            }
+            else
+            {
+                Response.Write("error in your input");
+            }
+        }
+    }
+
+}
