afd2bd946f8c8b9a97b33204fcc37ce0d621160a
Gayathri-Kolanu <71561916+Gayathri-Kolanu@users.noreply.github.com>
2020-11-22 12:12:25 +0530

    Add files via upload

---
+++ IWinDAL/ProductBestBeforeDate.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IWinDAL
+{
+    public class ProductBestBeforeDate
+    {
+        // establishing connection with the database
+        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["iwinConn"].ToString());
+        public DataSet GetNearExpiryProducts()
+        {
+            DataSet ds = new DataSet();
+            try
+            {
+                SqlCommand cmd = new SqlCommand("GetNearExpiryProducts_SP", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                SqlDataAdapter adp = new SqlDataAdapter(cmd);
+                adp.Fill(ds);
+                cmd.Dispose();
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            finally
+            {
+                ds.Dispose();
+            }
+            return ds;
+        }
+    }
+}
+
+
