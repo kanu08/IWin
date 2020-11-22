98e98cee4a6f500345197647067291a034758ad6
Gayathri-Kolanu <71561916+Gayathri-Kolanu@users.noreply.github.com>
2020-11-22 12:13:08 +0530

    Add files via upload

---
+++ IWinBO/ProductBestBeforeDate.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IWinBO
+{
+    public class ProductBestBeforeDate
+    {
+        public string ProductName { get; set; }
+        public string ProductDescription { get; set; }
+        public double Price { get; set; }
+        public int ProductId { get; set; }
+        public DateTime ProductExpiryDate { get; set; }
+
+
+    }
+}
