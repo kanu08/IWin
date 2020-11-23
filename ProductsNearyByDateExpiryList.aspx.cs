b6f310c83ff22759cda45932ba22c06c7342a599
Gayathri-Kolanu <71561916+Gayathri-Kolanu@users.noreply.github.com>
2020-11-23 11:41:01 +0530

    Best Before Section

---
+++ ProductsNearyByDateExpiryList.aspx.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using IWinBO;
+using IWinBLL;
+using System.Data;
+
+namespace IWin
+{
+    public partial class ProductsNearyByDateExpiryList : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                BindProductRecordsGridView();
+            }
+
+        }
+        private void BindProductRecordsGridView()
+        {
+            DataSet ds = new DataSet();
+            try
+            {
+                IWinBLL.Product objProduct= new IWinBLL.Product();
+                ds = objProduct.GetNearExpiryProducts();
+                if (ds.Tables[0].Rows.Count > 0)
+                {
+                    grdproductDetails.DataSource = ds;
+                    grdproductDetails.DataBind();
+                }
+                else
+                {
+                    grdproductDetails.DataSource = null;
+                    grdproductDetails.DataBind();
+                }
+            }
+            catch (Exception ex)
+            {
+                Response.Write(" error occured :" + ex.Message.ToString());
+            }
+            finally
+            {
+
+            }
+        }
+        #region "Paging in GridView"
+        protected void grdproductDetails_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            grdproductDetails.PageIndex = e.NewPageIndex;
+            BindProductRecordsGridView();
+        }
+        #endregion
+    }
+}
